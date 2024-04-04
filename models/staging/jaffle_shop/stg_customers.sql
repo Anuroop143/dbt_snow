266a71b209e3a0600cb2d8ba2318c1060470a1f7
venkataanuroop2@gmail.com <venkataanuroop2@gmail.com>
2024-04-04 12:14:45 +0000

    commit

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,5 +1,12 @@
+  with source as
+  (
+    select * from {{ source('jaffle_shop', 'customers') }}
+  ),
+  staged as(
   select 	
     ID as customer_id,
 	FIRST_NAME ,
 	LAST_NAME 
-     from {{ source('jaffle_shop', 'customers') }}
+     from source
+     )
+     select * from staged
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,6 +1,12 @@
-	select ID  as order_id,
+	with source as(
+        select * from {{ source('JAFFLE_SHOP', 'ORDERS') }}
+    ),
+    staged as (
+    select ID  as order_id,
 	USER_ID  as customer_id,
 	ORDER_DATE ,
 	STATUS ,
 	_ETL_LOADED_AT
-    from {{ source('JAFFLE_SHOP', 'ORDERS') }}
+    from source
+    )
+    select * from staged
