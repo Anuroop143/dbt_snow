b0f4ed8e5bc042ee60deab1be3f2efc325081089
Venkata sai anuroop samudrala <venkataanuroop2@gmail.com>
2024-04-05 05:40:50 +0000

    commit

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,12 +1,11 @@
-  with source as
-  (
-    select * from raw.jaffle_shop.customers
-  ),
-  staged as(
-  select 	
-    ID as customer_id,
-	FIRST_NAME ,
-	LAST_NAME 
-     from source
-     )
-     select * from staged
+with customers as (
+    
+    select 
+        id as customer_id,
+        first_name,
+        last_name
+
+    from raw.jaffle_shop.customers
+)
+
+select * from customers
